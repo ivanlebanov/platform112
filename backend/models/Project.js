c2ddb02cbe452735b55cfee3bdc97a8cbf754420
Anton Sotirov <anton.k.sotirov@gmail.com>
2020-03-28 18:49:14 +0200

    Added mongo Project.natural: mixed field

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 // Not sure
 const schema = mongoose.Schema({
-  hacker: String,
-  needs: String,
-  category: String,
-  team: String,
-  slack: String,
-  description: String,
-  solution: String
-})
+    hacker: String,
+    needs: String,
+    category: String,
+    team: String,
+    slack: String,
+    description: String,
+    solution: String,
+    natural: 'Mixed'
+});
 module.exports = mongoose.model('Project', schema, 'Project')
